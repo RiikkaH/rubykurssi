ca38bc17ceffe9ec3b2fb5226c31eea54228452e
RiikkaH <riikka.huusari@helsinki.fi>
2014-01-23 17:59:06 +0200

    viikko 2, t.14 tehty, 15 kesken

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,17 +1,9 @@
 class Beer < ActiveRecord::Base
+  
+  include RatingAverage
+
   belongs_to :brewery
   has_many :ratings, :dependent => :destroy
-
-  def average_rating
-    sum=0
-    howMany=0
-    ratings.each do |r|
-      sum=sum+r.score
-      howMany=howMany+1
-    end
-    sum/howMany
-
-  end
 
   def to_s
     "#{name}, #{Brewery.find(brewery_id).name}"
