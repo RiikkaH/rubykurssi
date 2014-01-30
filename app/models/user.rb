945d3158892ce9504c0eeee5a8e6701046b9da6e
RiikkaH <riikka.huusari@helsinki.fi>
2014-01-30 11:11:34 +0200

    viikkoa 3 eteenpäin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   include RatingAverage
+
+  has_secure_password
+
   validates :username, uniqueness: true,
                        length: { minimum: 3 },
                        length: { maximum: 15 }
+  validates :password, length: { minimum: 4 }
+                       
 
   has_many :ratings
+  has_many :memberships
 end
